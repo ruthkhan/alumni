956de8de554eaeefcf4f43246ffca296c67338c5
Ruth Khan <ruth.khan@gmail.com>
2024-02-20 10:35:24 -0800

    Added match page

--- alumnisite/mentormatch/serializers.py
+++ alumnisite/mentormatch/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import GradData
+from .models import MentorData
 
 class GradDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = GradData
+        fields = '__all__'
+
+class MentorDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MentorData
         fields = '__all__'
